2c47d76c36a9274df5b463569f330d19596600d7
pawelra1 <juniormax@onet.eu>
2018-07-19 23:12:11 +0200

    Portfolio w bootstrap

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+/* Efekt Smooth Scroll */
+
+$('a[href*="#"]:not([href="#"])').click(function(){
+//    console.log('test');
+    $('html, body').animate( { scrollTop: $(this.hash).offset().top - 75 }, 500 );
+});
+
+/* Scroll menu color change */
+
+if($(window).scrollTop() >= 50){
+    $('#main-nav').addClass('scroll');
+}
+
+$(window).scroll(function(){
+    if($(window).scrollTop() >= 50 ) {
+        $('#main-nav').addClass('scroll');
+        $('#main-nav').css({'transition':'all 0.3s'})
+    } else {
+        $('#main-nav').removeClass('scroll');
+    }
+});
